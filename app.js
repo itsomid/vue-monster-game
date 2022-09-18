e2c5cc675e67fc6b07398396066574a42c72a243
Omid Shabani <o.shabani@hotmail.com>
2022-09-18 12:38:08 +0430

    add methods

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+function getRandomValue(max, min){
+
+    return Math.floor(Math.random() * (max - min)) + min
+}
 const app = Vue.createApp({
-    
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+        }
+    },
+    methods: {
+        attackMonster(){
+            const attackValue = getRandomValue(12 , 5)
+            console.log(attackValue)
+            this.monsterHealth -= attackValue
+            this.attackPlayer() 
+        },
+        attackPlayer(){
+            const attackValue = getRandomValue(15 , 8)
+            this.playerHealth -= attackValue
+        }
+    },  
 })
 
 app.mount('#game')
